f916fbd8e53951a640b7449b4a4b2abd1a8c3f3a
MoreiraGu <santosmoreiragustavo7@gmail.com>
2026-03-18 21:38:03 -0300

    feat: criação da configuração inicial do banco de dados com projetos, usuarios e calculos

--- ForBack/apps/usuarios/models.py
+++ ForBack/apps/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome_usuario = models.CharField(max_length=120)
+    email_usuario = models.EmailField(max_length=150, unique=True)
+    senha_usuario = models.CharField(max_length=255)
+    nivel_usuario = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "usuarios"
+
+    def __str__(self):
+        return self.nome_usuario
--- ForBack/core/settings.py
+++ ForBack/core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.projetos',
     'apps.usuarios',
+    'apps.projetos',
     'apps.calculos',
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fornovo',
+        'USER': 'postgres',
+        'PASSWORD': 'fatec',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
