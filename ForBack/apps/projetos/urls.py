15aa1e582e4e001b547476155eb963a5a9a31032
Vlopes7 <viniciuslopes242005@gmail.com>
2026-03-19 15:58:40 -0300

    feat(SPACELINE-21): Implementação de classe 'cadastrarProjeto' com conexão com banco

--- ForBack/apps/projetos/urls.py
+++ ForBack/apps/projetos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import cadastrarProjeto
 app_name = 'projetos'
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+    path('cadastrarProjeto', views.cadastrarProjeto, name='cadastrarProjeto')
     
     # Exemplo: dominio.com/projetos/cadastrar/
     # path('cadastrar/', views.cadastrar_projeto, name='cadastrar'),
--- ForBack/apps/projetos/views.py
+++ ForBack/apps/projetos/views.py
@@ -3,12 +3,31 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .models import Projeto
+from apps.usuarios.models import Usuario
 
 class cadastrarProjeto(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        nome = request.data.get('nome')
-        engenheiro = request.data.get('engenheiro')
-        cliente = request.data.get('cliente')
-        localizacao = request.data.get('localizacao')
+        try:
+            engenheiro_id = request.data.get("engenheiro")
+
+            engenheiro = Usuario.objects.get(id=engenheiro_id)
+
+            projeto = Projeto.objects.create(
+                nome_projeto=request.data.get("nome_projeto"),
+                engenheiro=engenheiro,
+                cliente=request.data.get("cliente"),
+                localizacao=request.data.get("localizacao")
+            )
+
+            return Response({
+                "mensagem": "Projeto criado com sucesso",
+                "id": projeto.id_projeto
+            })
+
+        except Usuario.DoesNotExist:
+            return Response({"erro": "Engenheiro não encontrado"}, status=404)
+
+        except Exception as e:
+            return Response({"erro": str(e)}, status=400)
