f0b14bb4a56d6e5990e0bb66baa7b8e5884157b9
juliaprianti06 <juliaprianti3382@hotmail.com>
2026-04-22 21:55:39 -0300

    fix(SPACELINE-Relatorio): arrumando views em relação ao relatório

--- ForBack/apps/dados_ia/models.py
+++ ForBack/apps/dados_ia/models.py
@@ -91,5 +91,15 @@ class DadosInseridosManualmente(models.Model):
         db_table = "dados_inseridos_manualmente"
 
 class RelatorioConformidade(models.Model):
+    projeto = models.ForeignKey(
+        Projeto,
+        on_delete=models.CASCADE,
+        db_column="projeto_id"
+    )
+    nome_arquivo = models.CharField(max_length=255, default='')
+    caminho_arquivo = models.CharField(max_length=500, default='')
     arquivo = models.FileField(upload_to="relatorios/")
     criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "relatorios_conformidade"
