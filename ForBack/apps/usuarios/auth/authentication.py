b360fedf6aacdfe087d4a03daf6b11723fd45721
juliaprianti06 <juliaprianti3382@hotmail.com>
2026-04-28 09:07:00 -0300

    fix: corrigindo logout

--- ForBack/apps/usuarios/auth/authentication.py
+++ ForBack/apps/usuarios/auth/authentication.py
@@ -34,3 +34,6 @@ class CustomJWTAuthentication(BaseAuthentication):
             raise AuthenticationFailed('Usuário inativo.')
 
         return (usuario, token)
+
+    def authenticate_header(self, request):
+        return 'Bearer realm="api"'
